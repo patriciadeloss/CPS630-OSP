d79627f3b62ac02684085d3e33d861f479642dbd
nevisz <g1sanchez@torontomu.ca>
2025-02-19 18:15:36 -0500

    -Added header/navbar style to the rest of the php files. -Added subdirectories

---
+++ html/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Web Service Platform</title>
+    <link rel="stylesheet" href="../css/base-style.css">
+</head>
+<body>
+
+    <header id="top-navbar">
+        <div class="logo">
+            <img src="../img/dish.png" alt="Logo" class="logo-img">
+            Name
+        </div>
+
+        <form class="search-container" action="search.php" method="GET">
+            <input type="text" name="query" placeholder="Search">
+            <button type="submit">Go</button>
+        </form>
+
+        <div class="login">
+            <a href="cart.php" class="cart-icon">
+                <img src="../img/shopping-cart.png" alt="Cart">
+            </a>
+            <a href="signin.php" class="signin-btn">Sign In</a>
+        </div>
+    </header>
+
+    <div id="bottom-navbar">
+        <ul class="navlink">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="services.php">Types of Services</a></li>
+            <li><a href="reviews.php">Reviews</a></li>
+            <li><a href="about.php">About Us</a></li>
+        </ul>
+    </div>
+
+<!-- continuation in other php files -->
