52e2b9d4d1bdce3df4caaf3aeaa65d2409b88912
Patricia Delos Santos <pat.delossantos014@gmail.com>
2025-03-01 22:17:24 -0500

    Added status messages for Shopping Cart UI
    - Empty cart message for signed in user whose cart is empty
    - Sign in prompt for user who views Shopping Cart while not signed in

--- pages/cart.php
+++ pages/cart.php
@@ -2,35 +2,38 @@
     session_start();
     include("../external-php-scripts/database.php");
 
-    $userID = $_SESSION['user_id'];
 
-    // Differentiates the 2 ways a user navigates to the cart - dropping an item or
-    // simply clicking on the icon
-    // This if statement accounts for the first case
-    if (isset($_POST['itemID'])) {
-        // retrieve dropped item's id
-        $droppedItemID = $_POST['itemID']; 
-        // retrieve its corresponding entry in the Item table
-        $sql = "SELECT * FROM Item WHERE item_id = " . "$droppedItemID"; 
-        $result = $conn->query($sql);
-        $row = $result->fetch_assoc();
-        
-        $itemPrice = $row['price']; 
+    if (isset($_SESSION['account_type'])) {
+        $userID = $_SESSION['user_id'];
 
-        // update Shopping Cart table accordingly
-        $sql = "SELECT * FROM ShoppingCart WHERE item_id = " . "$droppedItemID";
-        $result = $conn->query($sql);
-
-        // if an instance of the item is already in the table, update its entry's price and qty
-        if ($result->num_rows > 0) {
-            $sql = "UPDATE ShoppingCart SET quantity = quantity+1, price = price+$itemPrice WHERE item_id = $droppedItemID";
+        // Differentiates the 2 ways a user navigates to the cart - dropping an item or
+        // simply clicking on the icon
+        // This if statement accounts for the first case
+        if (isset($_POST['itemID'])) {
+            // retrieve dropped item's id
+            $droppedItemID = $_POST['itemID']; 
+            // retrieve its corresponding entry in the Item table
+            $sql = "SELECT * FROM Item WHERE item_id = " . "$droppedItemID"; 
             $result = $conn->query($sql);
-        }
-        // if not, add a new entry w qty=1
-        // *** NOTE: order id is currently fixed !!
-        else {
-            $sql = "INSERT INTO ShoppingCart(order_id,item_id,user_id,quantity,price) VALUES(1,$droppedItemID,$userID,1,$itemPrice)";
+            $row = $result->fetch_assoc();
+            
+            $itemPrice = $row['price']; 
+
+            // update Shopping Cart table accordingly
+            $sql = "SELECT * FROM ShoppingCart WHERE item_id = " . "$droppedItemID";
             $result = $conn->query($sql);
+
+            // if an instance of the item is already in the table, update its entry's price and qty
+            if ($result->num_rows > 0) {
+                $sql = "UPDATE ShoppingCart SET quantity = quantity+1, price = price+$itemPrice WHERE item_id = $droppedItemID";
+                $result = $conn->query($sql);
+            }
+            // if not, add a new entry w qty=1
+            // *** NOTE: order id is currently fixed !!
+            else {
+                $sql = "INSERT INTO ShoppingCart(order_id,item_id,user_id,quantity,price) VALUES(1,$droppedItemID,$userID,1,$itemPrice)";
+                $result = $conn->query($sql);
+            }
         }
     }
 ?>
@@ -88,50 +91,69 @@
                     </tr>
                 </thead>
                 <tbody class="cart-container">
-                    <!-- display user's shopping cart items -->
                     <?php
-                        // retrieve a user's entries for their curr order from the Shopping Cart Table
-                        // *** NOTE: order id is currently fixed !!
-                        $sql = "SELECT * FROM ShoppingCart WHERE order_id = 1 AND user_id = $userID";
-                        $result = $conn->query($sql);
                         $grandTotal = 0.00;
                         $grandQty = 0;
+                    ?>
+                    <!-- If user is logged in, display cart -->
+                    <?php if (isset($_SESSION['account_type'])) { ?>
+                        <!-- display user's shopping cart items -->
+                        <?php
+                            // retrieve a user's entries for their curr order from the Shopping Cart Table
+                            // *** NOTE: order id is currently fixed !!
+                            $sql = "SELECT * FROM ShoppingCart WHERE order_id = 1 AND user_id = $userID";
+                            $result = $conn->query($sql);
 
-                        if ($result->num_rows > 0) {
-                            while ($cartRow = $result->fetch_assoc()) {
-                                // for the curr fetched item from the Cart table,
-                                
-                                // add its total price to the order total,
-                                $grandTotal = $grandTotal+$cartRow['price'];
-                                // increment item count of order,
-                                $grandQty = $grandQty+$cartRow['quantity'];
+                            if ($result->num_rows > 0) {
+                                while ($cartRow = $result->fetch_assoc()) {
+                                    // for the curr fetched item from the Cart table,
+                                    
+                                    // add its total price to the order total,
+                                    $grandTotal = $grandTotal+$cartRow['price'];
+                                    // increment item count of order,
+                                    $grandQty = $grandQty+$cartRow['quantity'];
 
-                                // and use its item_id to retrieve its item name from the Item table
-                                $sql = "SELECT * FROM Item WHERE item_id = " . $cartRow['item_id'];
-                                $result2 = $conn->query($sql); 
-                                $itemRow = $result2->fetch_assoc();
-                                
+                                    // and use its item_id to retrieve its item name from the Item table
+                                    $sql = "SELECT * FROM Item WHERE item_id = " . $cartRow['item_id'];
+                                    $result2 = $conn->query($sql); 
+                                    $itemRow = $result2->fetch_assoc();
+                                    
+                                    echo "
+                                        <tr>
+                                            <td class='child-left'>
+                                                <img src='../img/{$itemRow['image_url']}' alt=''>
+                                                <div id='itemInfo'>
+                                                    <h3>" . htmlspecialchars($itemRow['item_name']) . "</h3>
+                                                    <p>Item details</p>
+                                                </div>
+                                            </td>
+                                            <td id='price'>" . htmlspecialchars($itemRow['price']) . "</td>
+                                            <td id='amountSelector'>
+                                                <button>-</button>
+                                                <p id='amount'>" . htmlspecialchars($cartRow['quantity']) . "</p>
+                                                <button>+</button>
+                                            </td>
+                                            <td>" . htmlspecialchars($cartRow['price']) . "</td>
+                                        </tr>
+                                    ";
+                                }
+                            } else {
                                 echo "
                                     <tr>
-                                        <td class='child-left'>
-                                            <img src='../img/{$itemRow['image_url']}' alt=''>
-                                            <div id='itemInfo'>
-                                                <h3>" . htmlspecialchars($itemRow['item_name']) . "</h3>
-                                                <p>Item details</p>
-                                            </div>
-                                        </td>
-                                        <td id='price'>" . htmlspecialchars($itemRow['price']) . "</td>
-                                        <td id='amountSelector'>
-                                            <button>-</button>
-                                            <p id='amount'>" . htmlspecialchars($cartRow['quantity']) . "</p>
-                                            <button>+</button>
-                                        </td>
-                                        <td>" . htmlspecialchars($cartRow['price']) . "</td>
+                                        <td class=\"span-all\"> <p style=\"text-align: center;\"> Your Shopping Cart is Empty </p> </td>
                                     </tr>
                                 ";
                             }
-                        }
-                    ?>
+                        ?>
+                    
+                    <?php } else { ?>
+                    <!-- If not logged in, display message -->
+                    <tr>
+                        <td class="span-all"> 
+                            <p style="text-align: center;"> You are currently not signed in. <a href="signin.php">Sign in</a> to view your Shopping Cart </p> 
+                        </td>
+                    </tr>
+                <?php } ?>
                 </tbody>
             </table>
         </div>
@@ -142,7 +164,8 @@
                 <p>Tax: $<span id="tax">0.00</span></p>
             </div>
             <div id="p2">
-                <?php echo "<h2>Grand Total: $<span id='grandTotal'>{$grandTotal}</span></h2>" ?>
+                <!-- number_format to format 2 decimal places -->
+                <?php echo "<h2>Grand Total: $<span id='grandTotal'>". number_format($grandTotal, 2) . "</span></h2>" ?>
                 <button>Check Out</button>
             </div>
         </footer>
