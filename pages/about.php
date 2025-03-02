985f8d8a2e954e4a02d830e32cb7839e41acf079
nevisz <g1sanchez@torontomu.ca>
2025-03-02 01:15:34 -0500

    Created base style for About Us page

--- pages/about.php
+++ pages/about.php
@@ -5,9 +5,45 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Online Web Service Platform</title>
         <link rel="stylesheet" href="../css/base-style.css">
+        <link rel="stylesheet" href="../css/about.css">
     </head>
 
     <body>
         <?php include("header.php"); ?>
+
+        <section class="profile-card-container">
+            <article class="profile-card">
+                <section class="profile-card-header">
+                    <div class=pfp></div>
+                </section>
+                <section class="profile-card-body">
+                    <h2>Patricia Delos Santos</h2>
+                    <hr>
+                    <p>"Blah blah blah"<br>pdelos@torontomu.ca</p>
+                </section>
+            </article>
+            
+            <article class="profile-card">
+                <section class="profile-card-header">
+                    <div class=pfp></div>
+                </section>
+                <section class="profile-card-body">
+                    <h2>Genevive Sanchez</h2>
+                    <hr>
+                    <p>"Blah blah blah"<br>g1sanchez@torontomu.ca</p>
+                </section>
+            </article>
+
+            <article class="profile-card">
+                <section class="profile-card-header">
+                    <div class=pfp></div>
+                </section>
+                <section class="profile-card-body">
+                    <h2>Suboohi Sayeed</h2>
+                    <hr>
+                    <p>"Blah blah blah"<br>suboohi.sayeed@torontomu.ca</p>
+                </section>
+            </article>
+        </section>
     </body>
 </html>
